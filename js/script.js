a4bf9788ff86be7e9c547281b7bf7f0a66101f3e
Gabriela Machin <gabrielamachin.gm@gmail.com>
2022-03-19 11:28:41 -0300

    Desafío clase 5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 let viajeCalc = document.getElementById('viajeCalc');
 
+// Evento submit
 viajeCalc.addEventListener('submit', calcularGastos);
 
-function calcularGastos(e) {
-    e.preventDefault();
-    
+function obtenerValores() {
     let destino = document.getElementById('destino').value, 
         presupuesto = document.getElementById('presupuesto').value,
         alojamiento = document.getElementById('alojamiento').value,
         transporte = document.getElementById('transporte').value,
         comida = document.getElementById('comida').value;
+
+    return {destino, presupuesto, alojamiento, transporte, comida}
+}
+
+function calcularGastos(e) {
+    e.preventDefault();
+
+    const {destino, presupuesto, alojamiento, transporte, comida} = obtenerValores();
 
     let gastos = parseInt(alojamiento) + parseInt(transporte) + parseInt(comida);
     let balance = presupuesto - gastos;
@@ -24,13 +31,13 @@ function mostrarResultado(destino, presupuesto , balance) {
     datos.innerHTML = `
         <div class="container-datos row">
             <div class="col s4">
-                <h6>${destino}</h6>
+                <h6>Tu destino: ${destino}</h6>
             </div>
             <div class="col s4">
-                <h6>${presupuesto}</h6>
+                <h6>Tu presupuesto: ${presupuesto}</h6>
             </div>
             <div class="col s4">
-                <h6>${balance}</h6>
+                <h6>Tus gastos: ${balance}</h6>
             </div>
         </div>
     `
